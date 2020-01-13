87c40bb884258494996fc05e694df6813a3217b5
themooseking <55086952+themooseking@users.noreply.github.com>
2020-01-13 10:38:54 +0100

    controller

--- data/DatabaseController.java
+++ data/DatabaseController.java
@@ -1,6 +1,41 @@
 package data;
 
-public class DatabaseController {
+import java.util.ArrayList;
 
+import entities.Goal;
+import entities.Match;
+import entities.Suspension;
+import entities.Team;
+
+public class DatabaseController{
+	public void createTeam(String teamName) {
+		Team team = new Team(teamName);
+		DataLayer dataLayer = new DataLayer();
+		Teams teams = new Teams(dataLayer.getConnection());
+		
+		teams.createTeam(team);
+	}
+	public Team readTeamById(int id);
+	public void updateTeam(Team team);
+	public void deleteTeam(Team team);
+	public ArrayList<Team> getAllTeams();
 	
+	public void createSuspension(Suspension suspension);
+	public Team readSuspensionById(int id);
+	public void updateSuspension(Suspension suspension);
+	public void deleteSuspension(Suspension suspension);
+	public ArrayList<Suspension> getAllSuspensions(int matchID);
+	
+	public void createMatch(Match match);
+	public Team readMatchById(int id);
+	public void updateMatch(Match match);
+	public void deleteMatch(Match match);
+	public ArrayList<Match> getAllMatches();
+	
+	public void createGoal(Goal goal);
+	public Team readGoalById(int id);
+	public void updateGoal(Goal goal);
+	public void deleteGoal(Goal goal);
+	public ArrayList<Goal> getAllGoals(int matchID);
 }
+
