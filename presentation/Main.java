410e1827c797857f5bfb52e46316a1bea1c5a5b8
JamilChan <55086933+JamilChan@users.noreply.github.com>
2020-01-16 15:50:51 +0100

    .

--- presentation/Main.java
+++ presentation/Main.java
@@ -1,26 +1,28 @@
 package presentation;
 
+import data.DatabaseController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private DatabaseController dbController = new DatabaseController();
 
 	public static void main(String[] args) {
 
 		launch(args);
 	}
- 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		LoginMenu menu = new LoginMenu(primaryStage);
-		menu.init();
-	}  
 
 //	@Override
 //	public void start(Stage primaryStage) throws Exception {
-//		SpecificMatchMenu menu = new SpecificMatchMenu(primaryStage);
-//		menu.init("Hej Dean Title of Match", "Organizer");
-//
+//		LoginMenu menu = new LoginMenu(primaryStage);
+//		menu.init();
 //	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		SpecificMatchMenu menu = new SpecificMatchMenu(primaryStage, dbController.readMatchByIdNotPlayed(1));
+		menu.init("Hej Dean Title of Match", "Organizer");
+
+	}
 
 }
