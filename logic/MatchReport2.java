60be7ccbc27f8ca3e7369a303e9c7469b37f68d1
Dean <1thagok1@gmail.com>
2020-01-21 14:57:12 +0100

    Logic clean up

--- logic/MatchReport2.java
+++ logic/MatchReport2.java
@@ -12,26 +12,24 @@ public class MatchReport2 {
 
 	public ArrayList<SpecificMatchHistoryTable> matchReport(Match match) {
 		ArrayList<SpecificMatchHistoryTable> eventList = new ArrayList<>();
-		
+
 		String homeTeam = match.getHomeTeam().getTeamName();
 		String awayTeam = match.getAwayTeam().getTeamName();
-		
+
 		for (int i = 0; i < match.getGoalList().size(); i++) {
 			if (match.getGoalList().get(i).getScoringTeam().getTeamName() == homeTeam) {
 				String goalH = "Goal";
 				String goalA = "";
-				SpecificMatchHistoryTable event = new SpecificMatchHistoryTable(goalH,
-						matchTimeGoal(match, i), goalA);
+				SpecificMatchHistoryTable event = new SpecificMatchHistoryTable(goalH, matchTimeGoal(match, i), goalA);
 				eventList.add(event);
-				
+
 				homeTeamScore++;
 			} else if (match.getGoalList().get(i).getScoringTeam().getTeamName() == awayTeam) {
 				String goalA = "Goal";
 				String goalH = "";
-				SpecificMatchHistoryTable event = new SpecificMatchHistoryTable(goalH,
-						matchTimeGoal(match, i), goalA);
+				SpecificMatchHistoryTable event = new SpecificMatchHistoryTable(goalH, matchTimeGoal(match, i), goalA);
 				eventList.add(event);
-				
+
 				awayTeamScore++;
 			}
 		}
@@ -40,51 +38,49 @@ public class MatchReport2 {
 			if (match.getSuspensionList().get(i).getSuspensionTeam().getTeamName() == homeTeam) {
 				String susH = "Suspension";
 				String susA = "";
-				SpecificMatchHistoryTable event = new SpecificMatchHistoryTable(susH,
-						matchTimeSuspension(match, i), susA);
+				SpecificMatchHistoryTable event = new SpecificMatchHistoryTable(susH, matchTimeSuspension(match, i),
+						susA);
 				eventList.add(event);
 			} else if (match.getSuspensionList().get(i).getSuspensionTeam().getTeamName() == awayTeam) {
 				String susA = "Suspension";
 				String susH = "";
-				SpecificMatchHistoryTable event = new SpecificMatchHistoryTable(susH,
-						matchTimeSuspension(match, i), susA);
+				SpecificMatchHistoryTable event = new SpecificMatchHistoryTable(susH, matchTimeSuspension(match, i),
+						susA);
 				eventList.add(event);
 			}
 		}
-		
+
 		Collections.sort(eventList, new TimestampCompare());
 		return eventList;
 	}
+	
+	/***********************************
+	 * STRING GENERATORS
+	 ***********************************/
 
 	public String matchScore() {
-		String matchScore = Integer.toString(homeTeamScore) + " - " + Integer.toString(awayTeamScore);
-		
-		return matchScore;
+		return Integer.toString(homeTeamScore) + " - " + Integer.toString(awayTeamScore);
 	}
-	
+
 	private String matchTimeGoal(Match match, int i) {
 		long time = match.getGoalList().get(i).getMatchTime().getSeconds();
-		String timeStamp;
-		
-		if(time % 60 < 10) {
-			timeStamp = Long.toString(time / 60) + ":0" +Long.toString(time % 60);	
-		} else {
-			timeStamp = Long.toString(time / 60) + ":" +Long.toString(time % 60);
-		}
-		
-		return timeStamp;
+		return matchTimeString(time);
 	}
-	
+
 	private String matchTimeSuspension(Match match, int i) {
 		long time = match.getSuspensionList().get(i).getMatchTime().getSeconds();
+		return matchTimeString(time);
+	}
+
+	private String matchTimeString(long time) {
 		String timeStamp;
-		
-		if(time % 60 < 10) {
-			timeStamp = Long.toString(time / 60) + ":0" +Long.toString(time % 60);	
+
+		if (time % 60 < 10) {
+			timeStamp = Long.toString(time / 60) + ":0" + Long.toString(time % 60);
 		} else {
-			timeStamp = Long.toString(time / 60) + ":" +Long.toString(time % 60);
+			timeStamp = Long.toString(time / 60) + ":" + Long.toString(time % 60);
 		}
-		
+
 		return timeStamp;
 	}
 }
